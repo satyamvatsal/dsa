9ddbf36befb10889d08a21137660b34f0ef2e438
satyam <satyamvatsal257@gmail.com>
2023-10-02 06:22:44 +0530

    more readable binary search file

--- binary_search.cpp
+++ binary_search.cpp
@@ -4,16 +4,15 @@ using namespace std;
 int binser(int arr[], int n, int k) {
   int start = 0;
   int end = n - 1;
-  int mid;
+  int mid = start + (end - start) / 2;
   while (start <= end) {
-    mid = floor((start + end) / 2);
     if (arr[mid] > k)
       end = mid - 1;
     else if (arr[mid] < k)
       start = mid + 1;
     if (arr[mid] == k)
       return mid + 1;
-    mid = floor((start + end) / 2);
+    mid = start + (end - start) / 2;
   }
   return 0;
 }
@@ -21,7 +20,10 @@ int main() {
 
   int n, k;
   cin >> n >> k;
-  int arr[10];
+  int arr[n];
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
   int ans = binser(arr, n, k);
   if (ans) {
     cout << "Found at " << ans - 1 << endl;
